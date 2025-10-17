991d7a0a842886852ca9ad3eacefce3c63783e8c
Maha <mahawaqarwarraich@gmail.com>
2025-10-18 02:02:53 +0500

    zig zag arr

---
+++ zigzagarr.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void makeGreater(int gt, int sm, int& moves, vector<int>& nums) {
+    while (nums[sm] >= nums[gt]) {
+        nums[sm]--;
+        moves++;
+    }
+}
+
+int minEven(vector<int> nums) {
+    int moves = 0;
+    // set the target
+    int len = nums.size();
+    int target;
+    if (len%2 == 0) {
+        target = len-1;
+    } else {
+        target = len;
+    }
+    // main program
+    for (int i = 0; i < target; i++) {
+        if (i%2 == 0) {
+            if (i == 0 && nums[i+1] >= nums[i]) {
+               makeGreater(i, i+1, moves, nums);
+            } else if (i == len-1 && nums[i-1] >= nums[i]) {
+                makeGreater(i, i-1, moves, nums);
+            } else {
+                if (nums[i-1] >= nums[i]) {
+                    makeGreater(i, i-1, moves, nums);
+                } else if (nums[i+1] >= nums[i]) {
+                    makeGreater(i, i+1, moves, nums);
+                }
+            }
+        }
+    }
+
+    return moves;
+}
+
+int minOdd(vector<int> nums) {
+    int moves = 0;
+    // set the target
+    int len = nums.size();
+    int target;
+    if (len%2 == 0) {
+        target = len;
+    } else {
+        target = len-1;
+    }
+    // main program
+    for (int i = 0; i < target; i++) {
+        if (i%2 != 0) {
+            if (i == len-1 && nums[i-1] >= nums[i]) {
+               makeGreater(i, i-1, moves, nums);
+            }  else {
+                if (nums[i-1] >= nums[i]) {
+                    makeGreater(i, i-1, moves, nums);
+                } else if (nums[i+1] >= nums[i]) {
+                    makeGreater(i, i+1, moves, nums);
+                }
+            }
+        }
+    }
+
+    return moves;
+
+}
+
+int minMovs(vector<int> nums) {
+    int evenMovs = minEven(nums);
+    int oddMovs = minOdd(nums);
+
+    if (evenMovs < oddMovs) {
+        return evenMovs;
+    } else {
+        return oddMovs;
+    }
+}
+
+
+int main() {
+    vector<int> nums = {9,6,1,6,2};
+    int ans  = minMovs(nums);
+    cout << ans;
+    return 0;
+}
