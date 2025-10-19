45fc72267a9f28e2b20ccebadfe0f89eb399075d
Maha <mahawaqarwarraich@gmail.com>
2025-10-19 19:25:30 +0500

    optimized make greater function

--- zigzagarr.cpp
+++ zigzagarr.cpp
@@ -3,11 +3,23 @@
 using namespace std;
 
 void makeGreater(int gt, int sm, int& moves, vector<int>& nums) {
-    while (nums[sm] >= nums[gt]) {
-        nums[sm]--;
-        moves++;
+    // while (nums[sm] >= nums[gt]) {
+    //     nums[sm]--;
+    //     moves++;
+    // }
+
+
+    // My thinking
+    // int temp = nums[sm]-nums[gt];
+    // temp++;
+    // nums[sm] = temp;
+    // moves += temp;
+
+    // Correct
+    int requiredMoves = nums[sm] - (nums[gt] - 1);
+    nums[sm] = nums[gt] - 1;
+    moves += requiredMoves;
     }
-}
 
 int minEven(vector<int> nums) {
     int moves = 0;
